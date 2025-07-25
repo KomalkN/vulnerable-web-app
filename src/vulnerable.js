db8f3383be98084befd2b4c3df4e7fa9cf6b3140
Komal <144211225+KomalkN@users.noreply.github.com>
2025-07-25 11:35:37 +0530

    Update and rename test2.js to vulnerable.js

--- src/vulnerable.js
+++ src/vulnerable.js
@@ -4,3 +4,9 @@ app.post('/parse', (req, res) => {
   const obj = serialize.unserialize(data);
   res.send(obj);
 });
+
+
+app.get('/run', (req, res) => {
+  const code = req.query.code;
+  eval(code); // VERY DANGEROUS
+});
