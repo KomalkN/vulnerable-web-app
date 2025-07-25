90b45b5e9678da3e9f256183bf5207b9bda1006c
Komal <144211225+KomalkN@users.noreply.github.com>
2025-07-25 10:10:09 +0530

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,10 +1,10 @@
-const http = require('http');
-const { exec } = require('child_process');
+// const http = require('http');
+// const { exec } = require('child_process');
 
-http.createServer(function (req, res) {
-  const userInput = req.url.slice(1); // simulate user input from URL
-  exec(userInput, (error, stdout, stderr) => {
-    res.write(stdout);
-    res.end();
-  });
-}).listen(8080);
+// http.createServer(function (req, res) {
+//   const userInput = req.url.slice(1); // simulate user input from URL
+//   exec(userInput, (error, stdout, stderr) => {
+//     res.write(stdout);
+//     res.end();
+//   });
+// }).listen(8080);
