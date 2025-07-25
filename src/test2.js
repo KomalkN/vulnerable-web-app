463ba1604aa28f96473ad7176a1d5761cbe2352a
Komal <144211225+KomalkN@users.noreply.github.com>
2025-07-25 11:25:02 +0530

    Update test2.js

--- src/test2.js
+++ src/test2.js
@@ -1,4 +1,6 @@
-app.get('/run', (req, res) => {
-  const code = req.query.code;
-  eval(code); // VERY DANGEROUS
+const serialize = require('node-serialize');
+app.post('/parse', (req, res) => {
+  const data = req.body.data;
+  const obj = serialize.unserialize(data);
+  res.send(obj);
 });
