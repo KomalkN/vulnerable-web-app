cd892a31f386282dd7b37fd5b58a4df9b8fbea2f
Komal <144211225+KomalkN@users.noreply.github.com>
2025-07-25 22:27:46 +0530

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,12 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const serialize = require('node-serialize');
+const app = express();
+
+app.use(bodyParser.json());
+
 app.post('/parse', (req, res) => {
   const data = req.body.data;
-  const obj = serialize.unserialize(data);
+  const obj = serialize.unserialize(data);  // 🔥 Critical
   res.send(obj);
 });
 
-
 app.get('/run', (req, res) => {
   const code = req.query.code;
-  eval(code); // VERY DANGEROUS
+  eval(code);  // 🔥 Critical
+  res.send("Executed");
 });
+
+app.listen(3000, () => console.log("Listening on port 3000"));
