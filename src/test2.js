6dac5009c8bab4fba5c93b85a698d0f849b830cc
Komal <144211225+KomalkN@users.noreply.github.com>
2025-07-25 11:20:46 +0530

    Update test2.js

--- src/test2.js
+++ src/test2.js
@@ -1,1 +1,9 @@
-eval(userInput);
+const mysql = require('mysql');
+const connection = mysql.createConnection({ /* config */ });
+
+app.get('/user', (req, res) => {
+  const userId = req.query.id;
+  connection.query(`SELECT * FROM users WHERE id = '${userId}'`, (err, results) => {
+    res.send(results);
+  });
+});
